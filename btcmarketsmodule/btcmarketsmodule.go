4794dc096051672379e9a27819665649ae939ef0
Damien Buttler <damien@doublehops.com>
2020-05-02 13:35:06 +1000

    Properly formatting output.

--- btcmarketsmodule/btcmarketsmodule.go
+++ btcmarketsmodule/btcmarketsmodule.go
@@ -11,12 +11,8 @@ type handle struct {
 	btcclient *btcmarketsclient.Config
 }
 
-type CurrencyCollection struct {
-	CurrencyBalances []CurrencyBalance `json:"currencyBalance"`
-}
-
 type CurrencyBalance struct {
-	Balance        int     `json:"balance"`
+	Balance        float64 `json:"balance"`
 	PendingFunds   float64 `json:"pendingFunds"`
 	Currency       string  `json:"currency"`
 	CurrentPricing MarketData
@@ -49,49 +45,51 @@ func (h handle) PrintCurrencies() error {
 		return err
 	}
 
-	fmt.Println(currencyBalances)
-	fmt.Println("==========================================")
-
 	for _, cur := range currencyBalances {
 		currencyData, err := h.getCurrentPrices(cur.Currency)
 		if err != nil {
 			return err
 		}
-		fmt.Println("-----------------\nCurrency " + cur.Currency)
-		fmt.Println(currencyData)
 		cur.CurrentPricing = currencyData
-		fmt.Println(cur)
 	}
-	fmt.Println(currencyBalances)
 
-	//h.printValues(currencyBalances)
+	h.printValues(currencyBalances)
 
 	return nil
 }
 
-func (h handle) printValues(currencyBalances []CurrencyBalance) error {
+func (h handle) printValues(currencyBalances []*CurrencyBalance) error {
 
+	var total float64
+
+	fmt.Println("Cur\tBalance\t\tLast Price\tValue")
 	for _, cur := range currencyBalances {
-		if cur.Balance == 0 {
+		if cur.Balance == 0 || cur.Currency == "AUD" {
 			continue
 		}
-		value := float64(cur.Balance) * cur.CurrentPricing.LastPrice
-		fmt.Printf("Currency: %s\tBalance: %d\tLast Price: %f\tValue: %f\n", cur.Currency, cur.Balance, cur.CurrentPricing.LastPrice, value)
+		balance := cur.Balance / 100000000
+		value := balance * cur.CurrentPricing.LastPrice
+		total += value
+		valueStr := fmt.Sprintf("%.2f", value)
+		lastPriceStr := fmt.Sprintf("%.2f", cur.CurrentPricing.LastPrice)
+		fmt.Printf("%s\t%f\t%s\t%s\n", cur.Currency, balance, lastPriceStr, valueStr)
 	}
+
+	fmt.Printf("Total: %s\n", fmt.Sprintf("%.2f", total))
 
 	return nil
 }
 
-func (h handle) getAccountBalance() ([]CurrencyBalance, error) {
+func (h handle) getAccountBalance() ([]*CurrencyBalance, error) {
 	data, err := h.btcclient.MakeRequest("/account/balance")
 	if err != nil {
-		return []CurrencyBalance{}, err
+		return []*CurrencyBalance{}, err
 	}
 
-	currencies := make([]CurrencyBalance, 0)
+	currencies := make([]*CurrencyBalance, 0)
 	if err := json.Unmarshal(data, &currencies); err != nil {
 		fmt.Println("=== There is an error")
-		return []CurrencyBalance{}, err
+		return []*CurrencyBalance{}, err
 	}
 
 	return currencies, nil
